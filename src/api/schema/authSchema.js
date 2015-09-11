08d2a81c339f6093b8b67de4049a4a5a5174417a
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2015-09-11 22:03:22 +0200

    added auth schemas

---
+++ src/api/schema/authSchema.js
@@ -0,0 +1,32 @@
+import Joi from 'joi';
+
+export var tokenSchema = Joi
+  .object()
+  .keys({
+    token_type: Joi.string().required(),
+    access_token: Joi.string().guid().required(),
+    refresh_token: Joi.string().guid().required(),
+    expires_in: Joi.number().integer().required()
+  });
+
+export var authenticationSchema = Joi
+  .object()
+  .keys({
+    grant_type: Joi.string().required()
+  });
+
+export var passwordTypeSchema = authenticationSchema
+  .keys({
+    username: Joi.string().required(),
+    password: Joi.string().required()
+  });
+
+export var clientCredentialsTypeSchema = authenticationSchema
+  .keys({
+    user_id: Joi.string().guid().required()
+  });
+
+export var refreshTokenTypeSchema = authenticationSchema
+  .keys({
+    refresh_token: Joi.string().guid().required()
+  });

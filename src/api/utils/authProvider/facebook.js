59b124bcf082dea0e64f81f695e682347c017d4f
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2015-11-11 22:27:54 +0100

    added info for facebook authentication

---
+++ src/api/utils/authProvider/facebook.js
@@ -0,0 +1,9 @@
+
+/**
+ * a function that should validate a facebook token
+ */
+export function validate() {
+  // https://graph.facebook.com/me?access_token={accessToken}
+  // https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={accessToken}
+  //
+}

3b4d65874892ef485af9c39408da32120370050f
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2015-11-14 21:41:17 +0100

    deleted useless provider info

--- src/api/utils/authProvider/facebook.js
+++
@@ -1,9 +0,0 @@
-
-/**
- * a function that should validate a facebook token
- */
-export function validate() {
-  // https://graph.facebook.com/me?access_token={accessToken}
-  // https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={accessToken}
-  //
-}
